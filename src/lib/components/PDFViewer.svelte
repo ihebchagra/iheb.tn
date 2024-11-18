448fd37c2be63ad6d1714059fa18fb3e52d37e5f
Iheb Chagra <ihebchagra@gmail.com>
2024-11-18 20:19:38 +0100

    remove laggy code

--- src/lib/components/PDFViewer.svelte
+++ src/lib/components/PDFViewer.svelte
@@ -4,40 +4,40 @@
 	import { onMount, onDestroy } from 'svelte';
 	export let title;
 	export let pageNumber;
-	export let query = '';
+	// export let query = '';
 
 	let highlightInterval;
 
-	onMount(() => {
-		window.markInstance = new Mark('span');
+	// onMount(() => {
+	// 	window.markInstance = new Mark('span');
 
-		function initializeHighlighting() {
-			var pdfViewer = document.getElementById('pdf-viewer');
-			if (pdfViewer.contentDocument.readyState === 'complete') {
-				var iframeDocument = pdfViewer.contentDocument || pdfViewer.contentWindow.document;
-				var style = iframeDocument.createElement('style');
-				style.textContent = 'mark { background: red; color:red; opacity:20% }';
-				iframeDocument.head.appendChild(style);
-				window.markInstance = new Mark(iframeDocument);
-			}
-		}
+	// 	function initializeHighlighting() {
+	// 		var pdfViewer = document.getElementById('pdf-viewer');
+	// 		if (pdfViewer.contentDocument.readyState === 'complete') {
+	// 			var iframeDocument = pdfViewer.contentDocument || pdfViewer.contentWindow.document;
+	// 			var style = iframeDocument.createElement('style');
+	// 			style.textContent = 'mark { background: red; color:red; opacity:20% }';
+	// 			iframeDocument.head.appendChild(style);
+	// 			window.markInstance = new Mark(iframeDocument);
+	// 		}
+	// 	}
 
-		document.getElementById('pdf-viewer').addEventListener('load', initializeHighlighting);
+	// 	document.getElementById('pdf-viewer').addEventListener('load', initializeHighlighting);
 
-		highlightInterval = setInterval(function () {
-			if (window.markInstance) {
-				window.markInstance.mark(query, {
-					separateWordSearch: true,
-					accuracy: 'partially',
-					diacritics: true,
-					iframes: true,
-					ignorePunctuation: ':;.,-–—‒_(){}[]!\'"+='.split(''),
-					ignoreJoiners: true,
-					exclude: ['.marked']
-				});
-			}
-		}, 2000);
-	});
+	// 	highlightInterval = setInterval(function () {
+	// 		if (window.markInstance) {
+	// 			window.markInstance.mark(query, {
+	// 				separateWordSearch: true,
+	// 				accuracy: 'partially',
+	// 				diacritics: true,
+	// 				iframes: true,
+	// 				ignorePunctuation: ':;.,-–—‒_(){}[]!\'"+='.split(''),
+	// 				ignoreJoiners: true,
+	// 				exclude: ['.marked']
+	// 			});
+	// 		}
+	// 	}, 2000);
+	// });
 
 	onDestroy(() => {
 		if (highlightInterval) {
